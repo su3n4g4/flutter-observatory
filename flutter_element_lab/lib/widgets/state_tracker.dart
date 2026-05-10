8ce85bb096b4209d6a7e66f25a227b3f8ebed81e
suenaga <su3n4g4@gmail.com>
2026-05-10 12:02:22 +0900

    build内更新のコメント修正

--- flutter_element_lab/lib/widgets/state_tracker.dart
+++ flutter_element_lab/lib/widgets/state_tracker.dart
@@ -50,6 +50,8 @@ class _StateTrackerState extends State<StateTracker> {
 
   @override
   Widget build(BuildContext context) {
+    // rebuild の原因（setState / InheritedWidget 更新 / 親の更新）に関わらず
+    // 必ず呼ばれる場所が build() のみのため、ここでカウントする。
     _buildCount++;
     final element = context as Element; // BuildContextの実体はElement
     debugPrint(
